973017ad80c324e46b7920c16bdc67096714a160
humancore-joe <joe@humancore.ai>
2025-02-20 19:00:29 -0500

    repair template install to work with new versions of node, etc.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,131 +2,14 @@ import config from '../../config.js';
 const auth = config.BASIC_AUTH_USERNAME + ':' + config.BASIC_AUTH_PASSWORD;
 const ROUTES = [
   {
-    url: '/api/v1/batteries',
+    url: '/api/v1/recipes',
     proxy: {
       target: config.MSRVC_BATTERIES_URL,
       secure: false,
       auth: auth,
       changeOrigin: true,
     },
-  },
-  {
-    url: '/api/v1/content',
-    proxy: {
-      target: config.MSRVC_CONTENT_URL,
-      secure: false,
-      auth: auth,
-      changeOrigin: true,
-    },
-  },
-  {
-    url: '/api/v1/humans',
-    proxy: {
-      target: config.MSRVC_HUMANS_URL,
-      secure: false,
-      auth: auth,
-      changeOrigin: true,
-    },
-  },
-  {
-    url: '/api/v1/images',
-    proxy: {
-      target: config.MSRVC_IMAGES_URL,
-      secure: false,
-      auth: auth,
-      changeOrigin: true,
-    },
-  },
-  {
-    url: '/api/v1/ingestions',
-    proxy: {
-      target: config.MSRVC_INGESTION_URL,
-      secure: false,
-      auth: auth,
-      changeOrigin: true,
-    },
-  },
-  {
-    url: '/api/v1/organizations',
-    proxy: {
-      target: config.MSRVC_ORGANIZATIONS_URL,
-      secure: false,
-      auth: auth,
-      changeOrigin: true,
-    },
-  },
-  {
-    url: '/api/v1/pdf',
-    proxy: {
-      target: config.MSRVC_PDF_URL,
-      secure: false,
-      auth: auth,
-      changeOrigin: true,
-    },
-  },
-  {
-    url: '/api/v1/posts',
-    proxy: {
-      target: config.MSRVC_POSTS_URL,
-      secure: false,
-      auth: auth,
-      changeOrigin: true,
-    },
-  },
-  {
-    url: '/api/v1/questions',
-    proxy: {
-      target: config.MSRVC_QUESTIONS_URL,
-      secure: false,
-      auth: auth,
-      changeOrigin: true,
-    },
-  },
-  {
-    url: '/api/v1/responses',
-    proxy: {
-      target: config.MSRVC_RESPONSES_URL,
-      secure: false,
-      auth: auth,
-      changeOrigin: true,
-    },
-  },
-  {
-    url: '/api/v1/teams',
-    proxy: {
-      target: config.MSRVC_TEAMS_URL,
-      secure: false,
-      auth: auth,
-      changeOrigin: true,
-    },
-  },
-  {
-    url: '/api/v1/books',
-    proxy: {
-      target: config.MSRVC_TEMPLATE_URL,
-      secure: false,
-      auth: auth,
-      changeOrigin: true,
-    },
-  },
-  {
-    url: '/api/v1/tests',
-    proxy: {
-      target: config.MSRVC_TESTS_URL,
-      secure: false,
-      auth: auth,
-      changeOrigin: true,
-    },
-  },
-  {
-    url: '/api/v1/apps',
-    proxy: {
-      target: config.MSRVC_CONNECTED_APPS_URL,
-      secure: false,
-      auth: auth,
-      changeOrigin: true,
-    },
-  },
+  }
 ];
 
 export default ROUTES;
--- src/utils/setupProxies.js
+++ src/utils/setupProxies.js
@@ -1,7 +1,10 @@
-import {createProxyMiddleware} from 'http-proxy-middleware';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export default function setupProxies(app, routes) {
   routes.forEach(r => {
+    if (!r.proxy.target) {
+      r.proxy.target = 'http://localhost:8081'; 
+    }
     app.use(r.url, createProxyMiddleware(r.proxy));
   });
 }
